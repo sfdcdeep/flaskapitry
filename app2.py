d68b8e2423baf047d153028a94b258bb8d273d73
sfdcdeep <33954045+sfdcdeep@users.noreply.github.com>
2019-08-30 11:25:00 +0530

    Updated script to app2
    Just made changes nothing.

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from simple_salesforce import Salesforce
 sf = Salesforce(username='abatini27@gmail.com', password='Salesa@1234', security_token='4x8sp5Hi9tlbcBzfOCv2cP8N', sandbox=False)
 Lastname='Python'
-Firstname='Salesforce'
+Firstname='SFDC'
 Name = Firstname + " " + Lastname
 Accountcreation = sf.Account.create({'Name':Name})
 for k, v in Accountcreation.items():
